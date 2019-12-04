4ac3c31afee1a3798931ed00f875aa50a5de3e17
xiewei <1334927929@qq.com>
2019-12-04 14:29:55 +0800

    first commit1

--- dist/js/chunk-6542b16a.7919700d.js
+++ dist/js/chunk-6542b16a.7919700d.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-6542b16a"],{"29db":function(t,n,e){"use strict";var a=e("a5d2"),r=e.n(a);r.a},"8b42":function(t,n,e){t.exports=e.p+"img/not.a4ed1409.jpeg"},a5d2:function(t,n,e){},be4d:function(t,n,e){"use strict";e.r(n);var a=function(){var t=this,n=t.$createElement,a=t._self._c||n;return a("div",{staticClass:"not-found"},[a("van-nav-bar",{attrs:{title:"","left-text":"返回","left-arrow":""},on:{"click-left":function(n){return t.$router.go(-1)}}}),a("img",{attrs:{src:e("8b42"),alt:""}})],1)},r=[],o={},c=o,u=(e("29db"),e("2877")),i=Object(u["a"])(c,a,r,!1,null,null,null);n["default"]=i.exports}}]);
-//# sourceMappingURL=chunk-6542b16a.1e2f3233.js.map
+//# sourceMappingURL=chunk-6542b16a.7919700d.js.map
